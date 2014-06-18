7e2780df52fa8b3658a597493846b0842682ae9a
Tristan Lins <tristan.lins@bit3.de>
2014-06-18 15:25:02 +0200

    Extract specific translations from core package.

---
+++ module/languages/en/tl_xnavigation_menu.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * xNavigation - Highly extendable and flexible navigation module for the Contao Open Source CMS
+ *
+ * Copyright (C) 2013 bit3 UG <http://bit3.de>
+ *
+ * @package    xNavigation
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @link       http://www.themeplus.de
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_root']       = array(
+	'Root page',
+	'Please select the root page for the menu.'
+);
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_root_level'] = array(
+	'Start level',
+	'Please enter the start level of the menu.'
+);
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_root_id']    = array(
+	'Custom root page',
+	'Please select the custom root page for the menu.'
+);
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_root_ids']   = array(
+	'Individual pages',
+	'Please select the individual pages for the menu.'
+);
+
+/**
+ * Reference
+ */
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['roots']['page']            = array('Page');
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_roots']['root']       = array(
+	'Root page',
+	'Use the root page of the website as start point.'
+);
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_roots']['parent']     = array(
+	'Parent page',
+	'Use the parent page of the currently active page as start point.'
+);
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_roots']['current']    = array(
+	'Current page',
+	'Use the current active page as start point.'
+);
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_roots']['level']      = array(
+	'Specific level',
+	'Define a level in the trail as start point, beginning by 0.'
+);
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_roots']['custom']     = array(
+	'Custom page',
+	'Use a custom selected page as start point.'
+);
+$GLOBALS['TL_LANG']['tl_xnavigation_menu']['page_roots']['individual'] = array(
+	'Individual',
+	'Individual ordered collection of multiple pages.'
+);
